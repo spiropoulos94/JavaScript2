f5017176b4b62ca3a061914618fe3499b14a3238
Nikos Spiropoulos <spiropoulos94@gmail.com>
2020-02-01 13:54:15 +0200

    Update takeOutTheDuplicates.js

--- Week3/js-exercises-week3/takeOutTheDuplicates.js
+++ Week3/js-exercises-week3/takeOutTheDuplicates.js
@@ -1,10 +1,16 @@
 const letters = ['a', 'b', 'c', 'd', 'a', 'e', 'f', 'c', 'b'];
 
 
-function removeDuplicates(array) {
+//Made first solution using Set method.
+
+/*function removeDuplicates(array) {
     let uniqArr = Array.from(new Set(array))
     return uniqArr
-}
+}*/
 
+function removeDuplicates(array) {
+    return array.filter((letter, index) => array.indexOf(letter) === index)
+};
 
+removeDuplicates(letters);
 
