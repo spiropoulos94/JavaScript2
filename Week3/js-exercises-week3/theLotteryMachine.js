1dc2e07c4d0e2c99321e5a98189d8e1f6fe69a03
Nikos Spiropoulos <spiropoulos94@gmail.com>
2020-02-02 12:33:44 +0200

    Update theLotteryMachine.js

--- Week3/js-exercises-week3/theLotteryMachine.js
+++ Week3/js-exercises-week3/theLotteryMachine.js
@@ -1,15 +1,45 @@
-function threeFive(startIndex, stopIndex, ) {
+function sayThree() {
+    console.log("Say Three is called!!!!")
+}
+
+function sayFive() {
+    console.log("Say Five is called!!!!")
+}
+
+
+
+function threeFive(startIndex, stopIndex, sayThree, sayFive) {
     const numbers = [];
     // make array
     for (let i = startIndex; i < stopIndex + 1; i++) {
         numbers.push(i)
 
     }
-    console.log(numbers)
+
+
+
+
+    for (let i of numbers) {
+
+        if (i % 3 == 0 && i % 5 == 0) {
+            sayThree();
+            sayFive();
+        }
+
+        else if (i % 3 == 0) {
+            sayThree();
+        }
+        else if (i % 5 == 0) {
+            sayFive();
+        }
+
+    }
+
+
 }
 
 // start at beginning of array and check if you should call threeCallback or fiveCallback or go on to next
 
 
-threeFive(10, 150);
+threeFive(10, 15, sayThree, sayFive);
 
